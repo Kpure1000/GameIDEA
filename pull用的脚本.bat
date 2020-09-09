48b02c21b2df50aff72987d06c2ec867df5f81f7
Kpurek <ktsq2000@126.com>
2020-09-10 00:12:38 +0800

    auto-commit by bat command

--- pull用的脚本.bat
+++ pull用的脚本.bat
@@ -1,18 +1,31 @@
-@echo check for Git...
-git version
 @echo **************************
-@echo if Git's not been installed, please EXIT.
+@echo This is an AUTO PULL script for GitHub
+@echo Author @Kpure1000 https://github.com/Kpure1000
+@echo **************************
+@echo Check for Git...[if NOT installed, just EXIT]
+@echo off
+@FOR /F "delims=" %%I IN (
+"git.exe" ) DO (
+	@if exist %%~$PATH:I (
+		@echo DO exist Git 
+		@git version
+	) else ( echo Git has been not INSTALL or not in PATH.
+			@pause	)
+	)
+@echo **************************
+@echo Being READY to PULL!
 @pause
 @echo **************************
 @cd %userprofile%\Desktop
 @if exist GameIDEA\ (
 	@echo exist GameIDEA folder, pull it down...
 	@cd .\GameIDEA\
+	@git add -A
+	@git PULL origin master
 	@start .
-	git pull origin master
 ) else (
 	@echo NOT exist GameIDEA folder, clone one...
-	git clone https://github.com/Kpure1000/GameIDEA.git
+	@git clone https://github.com/Kpure1000/GameIDEA.git
 	@cd .\GameIDEA\
 	@start .
 )
--- push用的脚本.bat
+++ push用的脚本.bat
@@ -1,29 +1,29 @@
-@echo This is an auto push script
-@echo @author Kpure1000
 @echo **************************
-@echo check for Git...[if NOT installed, just EXIT]
-@FOR /F "delims=" %%I 
-@IN (
-"git.exe" ) 
-@DO (
+@echo This is an AUTO push script
+@echo Author @Kpure1000 https://github.com/Kpure1000
+@echo **************************
+@echo Check for Git...[if NOT installed, just EXIT]
+@echo off
+@FOR /F "delims=" %%I IN (
+"git.exe" ) DO (
 	@if exist %%~$PATH:I (
-		echo DO exist Git 
+		@echo DO exist Git 
 		@git version
 	) else ( echo Git has been not INSTALL or not in PATH yet.
 			@pause	)
 	)
 @echo **************************
-@echo Already going to PUSH!
+@echo Being READY to PUSH!
 @pause
 @echo **************************
 @cd %userprofile%\Desktop
 @if exist GameIDEA\ (
 	@echo exist GameIDEA folder, pull it down...
 	@cd .\GameIDEA\
-	@start .
 	git add -A
 	git commit -m "auto-commit by bat command"
 	git push origin master
+	@start .
 ) else (
 	@echo NOT exist GameIDEA folder, clone one...
 	git clone https://github.com/Kpure1000/GameIDEA.git
